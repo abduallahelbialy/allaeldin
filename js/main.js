1c5b1a33ebadff77a54a510052c68d8758622f08
abduallahelbialy <abduallahelbialy@gmail.com>
2025-07-01 15:14:43 +0300

    eidt in iput pass

--- js/main.js
+++ js/main.js
@@ -96,18 +96,18 @@ tabs.forEach((tab) => {
     });
       ///purpleBox///
 //login page///
-function togglePassword() {
-  const passwordInput = document.getElementById("password");
-  const toggleIcon = document.querySelector(".toggle-password");
+// function togglePassword() {
+//   const passwordInput = document.getElementById("password");
+//   const toggleIcon = document.querySelector(".toggle-password");
 
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-    toggleIcon.textContent = "🙈";
-  } else {
-    passwordInput.type = "password";
-    toggleIcon.textContent = "👁️"; 
-  }
-}
+//   if (passwordInput.type === "password") {
+//     passwordInput.type = "text";
+//     toggleIcon.textContent = "🙈";
+//   } else {
+//     passwordInput.type = "password";
+//     toggleIcon.textContent = "👁️"; 
+//   }
+// }
 
 
 
